b69065f1e5b7f8563c1c5535d61e6571377ddec2
sdaza7 <54986526+sdaza7@users.noreply.github.com>
2021-02-26 12:49:25 -0500

    Update hello.cpp
    Se escribe el cómo se desarrolla el proceso del 2*a

--- hello.cpp
+++ hello.cpp
@@ -2,6 +2,8 @@
 #include <cstdlib>
 int main(int argc, char **argv)
 {
+    // ./a.out -> argc = 1, argv = ["./a.out"]
+    // ./a.out 10 -> argc = 2, argv = ["./a.out", "10"]
     int a = std::atoi(argv[1]);
     a=2*a;
     std::cout << "Hello World" << "\n";
