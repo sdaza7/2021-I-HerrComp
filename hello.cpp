cead44b089eba78713eb5986f3bd7f28bf42e48c
Samuel ALejandro Daza Rodriguez <sdaza@unal.edu.co>
2021-02-26 12:44:02 -0500

    Ahora imprime también el doble del numero que se agrege en la cosola

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
-int main(int arg, char **argv)
+#include <cstdlib>
+int main(int argc, char **argv)
 {
+    int a = std::atoi(argv[1]);
+    a=2*a;
     std::cout << "Hello World" << "\n";
+    std::cout << "2*a= " << a << "\n";
     return 0;
 }
