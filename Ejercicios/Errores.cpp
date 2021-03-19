fa1d218a248b40eaa3cc9e4b4b51bf08689082cb
Samuel ALejandro Daza Rodriguez <sdaza@unal.edu.co>
2021-03-19 18:15:12 -0500

    ESta es la segunda version del codigo

--- Ejercicios/Errores.cpp
+++ Ejercicios/Errores.cpp
@@ -2,45 +2,22 @@
 #include <cmath>
 
 typedef double REAL;
-REAL x = 2.0;
-REAL cen = 0.0;
+REAL x = 1.0;
+REAL cen = x;
 REAL pot = x - cen;
 
 REAL f1(REAL x);
 REAL factorial(int N);
 
 REAL S1 = 0.0;
-REAL val = x - 1.0;
-REAL inter = cen - 1.0;
+REAL val = 1.0 - x;
+REAL inter = 1.0 - cen;
 int main (void)
 {
     std::cout.precision(15);
     std::cout.setf(std::ios::scientific);
-    //Serie de Taylor con pows
-    int d = 0;
-    int e = 0;
-    int g = 0;
-    int h = 0;
-    for(int ii = 0; ii < 8; ++ii)
-    {
-        d = 10-ii;
-        e = 9-ii;
-        h = e-1;
-        if (ii==0)
-        {
-            S1 = std::pow(inter,8);
-        }
-        else if (ii == 1)
-        {
-            S1 = S1 + 8*std::pow(inter,7)*std::pow(pot,ii)/factorial(ii);
-        }
-        else
-        {
-            g = d*e;
-            S1 += g*std::pow(inter,h)*std::pow(pot,ii)/factorial(ii);
-        }
-    }
-
+    //Serie de Taylor con Pows
+    S1 = std::pow(pot,8) - 8*std::pow(pot,7)*x/factorial(1) + 56*std::pow(pot,6)*std::pow(x,2)/factorial(2) - 6*56*std::pow(pot,5)*std::pow(x,3)/factorial(3) + 30*56*std::pow(pot,4)*std::pow(x,4)/factorial(4) - 120*56*std::pow(pot,3)*std::pow(x,5)/factorial(5) + 360*56*std::pow(pot,2)*std::pow(x,6)/factorial(6) - 360*112*std::pow(pot,1)*std::pow(x,7)/factorial(7) + 360*112*std::pow(x,8)/factorial(8);
     std::cout << f1(x) << "\t" << S1 << "\n";
     return 0;
 }
