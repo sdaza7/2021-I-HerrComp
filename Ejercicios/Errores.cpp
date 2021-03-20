0fc03a9aeedc6ab78accdc4d82f63d57d9e39fca
Samuel ALejandro Daza Rodriguez <sdaza@unal.edu.co>
2021-03-19 19:05:04 -0500

    Errores corregidos para S1, queda corregir S2

--- Ejercicios/Errores.cpp
+++ Ejercicios/Errores.cpp
@@ -1,24 +1,30 @@
 #include <iostream>
 #include <cmath>
+#include <cstdlib>
 
 typedef double REAL;
-REAL x = 1.0;
-REAL cen = x;
-REAL pot = x - cen;
 
-REAL f1(REAL x);
+REAL f1(REAL b);
 REAL factorial(int N);
 
-REAL S1 = 0.0;
-REAL val = 1.0 - x;
-REAL inter = 1.0 - cen;
-int main (void)
+int main (int argc, char **argv)
 {
+    //X se da por el Usuario en el Copilador
+    REAL x = std::atof(argv[1]) ;
+    //Al ser una serie de Taylor, está centrada en Cero, por lo tanto:
+    REAL cen = 0.0;
+    //Pot corresponde al termino (1-cen), que se repite en las derivadas a reemplazar el valor de CEN en estas.
+    REAL pot = 1 - cen;
     std::cout.precision(15);
     std::cout.setf(std::ios::scientific);
     //Serie de Taylor con Pows
+    REAL S1 = 0.0;
     S1 = std::pow(pot,8) - 8*std::pow(pot,7)*x/factorial(1) + 56*std::pow(pot,6)*std::pow(x,2)/factorial(2) - 6*56*std::pow(pot,5)*std::pow(x,3)/factorial(3) + 30*56*std::pow(pot,4)*std::pow(x,4)/factorial(4) - 120*56*std::pow(pot,3)*std::pow(x,5)/factorial(5) + 360*56*std::pow(pot,2)*std::pow(x,6)/factorial(6) - 360*112*std::pow(pot,1)*std::pow(x,7)/factorial(7) + 360*112*std::pow(x,8)/factorial(8);
-    std::cout << f1(x) << "\t" << S1 << "\n";
+    //Serie de TAylor con productos
+    REAL S2 = 0.0;
+    S2 = 1 - 1*x/factorial(1) + 56*x*x/factorial(2) - 6*56*x*x*x/factorial(3) + 30*56*x*x*x*x/factorial(4) - 120*56*x*x*x*x*x/factorial(5) + 360*56*x*x*x*x*x*x/factorial(6) - 360*112*x*x*x*x*x*x*x/factorial(7) + 360*112*x*x*x*x*x*x*x*x/factorial(8);
+    std::cout << "X" << "\t" << "Pow" << "S Taylor" << "S Taylor Pow" << "\n";
+    std::cout << x <<  "\t" << f1(x) << "\t" << S1 << "\t" << S2 << "\n";
     return 0;
 }
 
